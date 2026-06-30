db046cdc78418fd2f9d24a09181373371eac2f69
Adhan-Hashim <adhanhashim@gmail.com>
2026-06-30 06:39:14 +0530

    style: remove footer color separation, force white text, update instagram text

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -17,7 +17,7 @@ export default function Footer() {
 
         <div className="ss-right">
           <div className="ss-contact">
-            <p>instagram @chattipathal</p>
+            <p>@chattipathal</p>
             <p>hello@okayspace.com | okayspace.com</p>
           </div>
         </div>
