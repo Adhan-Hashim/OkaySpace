8a395937bb38fa983bda4209fc74f75114aaa8df
Adhan-Hashim <adhanhashim@gmail.com>
2026-06-30 21:11:58 +0530

    Fix mobile margin overlaps and background anchor

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import App from './App.tsx';
+import './index.css'; // Updated CSS
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// Force a new build hash
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
